e07245f84a413776ef1fc5e0a1d3ecb2701ec98f
Colin Chauche <colin.chauche@gmail.com>
2021-12-07 00:02:32 -0500

    Partial part 2 solution

--- day04/day04.exs
+++ day04/day04.exs
@@ -6,18 +6,16 @@ defmodule Day04 do
   def check(_), do: false
 
   def check_rows(board) do
-    row_wins? =
-      board
-      |> Enum.chunk_every(5)
-      |> Enum.any?(&check/1)
+    board
+    |> Enum.chunk_every(5)
+    |> Enum.any?(&check/1)
   end
 
   def check_cols(board) do
-    col_wins? =
-      board
-      |> Enum.chunk_every(5)
-      |> List.zip()
-      |> Enum.any?(&check/1)
+    board
+    |> Enum.chunk_every(5)
+    |> List.zip()
+    |> Enum.any?(&check/1)
   end
 
   def update_boards(boards, num) do
@@ -26,7 +24,7 @@ defmodule Day04 do
         board, nil ->
           board =
             Enum.map(board, fn
-              {el, x} when el == num -> {el, 1}
+              {el, _x} when el == num -> {el, 1}
               el -> el
             end)
 
@@ -63,6 +61,65 @@ defmodule Day04 do
         find_winning_board(boards, nums)
     end
   end
+
+  def find_last_winning_board(boards, [num | nums]) do
+    case update_remove_winners(boards, num) do
+      [board] ->
+        case check_rows(board) || check_cols(board) do
+          false ->
+            find_last_winning_board(board, nums)
+
+          _ ->
+            {board, String.to_integer(num)}
+        end
+
+      boards ->
+        find_last_winning_board(boards, nums)
+    end
+  end
+
+  def find_last_winning_board(_, [], winners), do: winners
+
+  def update_remove_winners([board | []], num) do
+    board =
+      Enum.map(board, fn
+        {el, _x} when el == num -> {el, 1}
+        el -> el
+      end)
+
+    [board]
+  end
+
+  def update_remove_winners([board | boards], num) do
+    board =
+      Enum.map(board, fn
+        {el, _x} when el == num -> {el, 1}
+        el -> el
+      end)
+
+    case check_rows(board) || check_cols(board) do
+      true ->
+        IO.inspect(Enum.chunk_every(board, 5), label: "Tossing")
+        update_remove_winners(boards, num)
+
+      _ ->
+        [board | update_remove_winners(boards, num)]
+    end
+  end
+
+  def calc_result(board, num) do
+    sum =
+      board
+      |> Enum.reduce(0, fn
+        {num, 0}, acc ->
+          String.to_integer(num) + acc
+
+        _el, acc ->
+          acc
+      end)
+
+    sum * num
+  end
 end
 
 test_nums = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"
@@ -87,8 +144,8 @@ test_boards = """
  2  0 12  3  7
 """
 
-test_boards = File.read!("boards.txt")
-test_nums = File.read!("numbers.txt")
+# test_boards = File.read!("boards.txt")
+# test_nums = File.read!("numbers.txt")
 
 test_boards =
   test_boards
@@ -106,16 +163,11 @@ test_nums =
 
 # |> IO.inspect(label: "Nums")
 
-{board, last_num} = Day04.find_winning_board(test_boards, test_nums)
+{board, num} = Day04.find_winning_board(test_boards, test_nums)
+{last_board, last_num} = Day04.find_last_winning_board(test_boards, test_nums)
 
-board_sum =
-  board
-  |> Enum.reduce(0, fn
-    {num, 0}, acc ->
-      String.to_integer(num) + acc
+IO.inspect(Enum.chunk_every(last_board, 5), label: "last_board")
+IO.inspect(last_num, label: "last_num")
 
-    _el, acc ->
-      acc
-  end)
-
-IO.inspect(board_sum * last_num, label: "Part 1 Result")
+IO.inspect(Day04.calc_result(board, num), label: "Part 1 Result")
+IO.inspect(Day04.calc_result(last_board, last_num), label: "Part 2 Result")
